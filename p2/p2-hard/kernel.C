f7e50fbd5e359cb2907df00ab40e974240a36555
Vincent Tsang <yukun.zeng.hit@outlook.com>
2016-10-09 17:12:58 -0500

    init commit with P2 partial test case, P2-naive completed, P2-hard partial

---
+++ p2/p2-hard/kernel.C
@@ -0,0 +1,197 @@
+/*
+    File: kernel.C
+
+    Author: R. Bettati
+            Department of Computer Science
+            Texas A&M University
+    Date  : 12/09/03
+
+
+    This file has the main entry point to the operating system.
+
+*/
+
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+#define MB * (0x1 << 20)
+#define KB * (0x1 << 10)
+#define KERNEL_POOL_START_FRAME ((2 MB) / (4 KB))
+#define KERNEL_POOL_SIZE ((2 MB) / (4 KB))
+#define PROCESS_POOL_START_FRAME ((4 MB) / (4 KB))
+#define PROCESS_POOL_SIZE ((28 MB) / (4 KB))
+/* definition of the kernel and process memory pools */
+
+#define MEM_HOLE_START_FRAME ((15 MB) / (4 KB))
+#define MEM_HOLE_SIZE ((1 MB) / (4 KB))
+/* we have a 1 MB hole in physical memory starting at address 15 MB */
+
+#define FAULT_ADDR (4 MB)
+/* used in the code later as address referenced to cause page faults. */
+#define NACCESS ((1 MB) / 4)
+/* NACCESS integer access (i.e. 4 bytes in each access) are made starting at address FAULT_ADDR */
+#define ERROR 0xffffffff
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "machine.H"     /* LOW-LEVEL STUFF   */
+#include "console.H"
+//#include "gdt.H"
+//#include "idt.H"          /* LOW-LEVEL EXCEPTION MGMT. */
+//#include "irq.H"
+//#include "exceptions.H"
+#include "interrupts.H"
+#include "frame_pool.H"
+//#include "simple_timer.H" /* TIMER MANAGEMENT */
+
+//#include "page_table.H"
+//#include "paging_low.H"
+
+/*--------------------------------------------------------------------------*/
+/* MAIN ENTRY INTO THE OS */
+/*--------------------------------------------------------------------------*/
+
+int main() {
+
+    Console::init();
+
+
+
+
+    /* -- INITIALIZE FRAME POOLS -- */
+
+    FramePool kernel_mem_pool(KERNEL_POOL_START_FRAME,
+                              KERNEL_POOL_SIZE,
+                              0);
+    unsigned long process_mem_pool_info_frame = kernel_mem_pool.get_frame();
+    FramePool process_mem_pool(PROCESS_POOL_START_FRAME,
+                               PROCESS_POOL_SIZE,
+                               process_mem_pool_info_frame);
+    process_mem_pool.mark_inaccessible(MEM_HOLE_START_FRAME, MEM_HOLE_SIZE);
+	//test
+    /* -- MOST OF WHAT WE NEED IS SETUP. THE KERNEL CAN START. */
+
+    Console::puts("Hello World, we are conquering P2!\n");
+
+    /* HERE WRITE YOUR OWN CODE TO TEST THE FRAME POOL CLASS
+     * IMPLEMENTATION WITH THE TWO OBJECTS WE ALLOCATED - ON THE
+     * STACK, WE DO NOT HAVE "NEW" YET - ABOVE */
+    //TODO
+    //test get
+    int pass = 0;
+    Console::puts("===================Kernel Mem Pool Test===============\n");
+    ///////////////////
+    Console::puts("Direct Release:\t");
+    if(kernel_mem_pool.release_frame(1) == ERROR){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else
+        Console::puts("Failed\n");
+    /////////////////
+    Console::puts("Mark Inaccessible:\t");
+    if(kernel_mem_pool.mark_inaccessible(555, 22) != ERROR){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else
+        Console::puts("Failed\n");
+    /////////////////
+    Console::puts("Get All Frame:\t");
+    int failed = 0;
+    for(int i = KERNEL_POOL_START_FRAME; i < KERNEL_POOL_START_FRAME +
+        KERNEL_POOL_SIZE; i++) // allocate it all again
+    {
+        if(kernel_mem_pool.get_frame() == ERROR)
+            ++failed;
+    }
+    if(failed == 22){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else{
+        Console::puti(failed);
+        Console::puts(" Failure\n");
+    }
+    //
+    Console::puts("Get A Frame:\t");
+    if(kernel_mem_pool.get_frame() == ERROR){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else
+        Console::puts("Failed\n");
+    //
+    Console::puts("Out Boundary Release\t");
+    if(kernel_mem_pool.release_frame(1024) == ERROR){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else
+        Console::puts("Failed\n");
+    //release
+    Console::puts("Boundary Release\t");
+    if(kernel_mem_pool.release_frame(1023) != ERROR){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else
+        Console::puts("Failed\n");
+    //
+    Console::puts("Multi Release\t");
+    if(kernel_mem_pool.release_frame(1023) == ERROR){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else
+        Console::puts("Failed\n");
+    //get just released frame
+    Console::puts("Get Just Released\t");
+    if(kernel_mem_pool.get_frame() == 1023){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else
+        Console::puts("Failed\n");
+    //release all frames
+    Console::puts("Release All\t");
+    failed = 0;
+    for(int i = KERNEL_POOL_START_FRAME; i < KERNEL_POOL_START_FRAME +
+        KERNEL_POOL_SIZE; i++) // allocate it all again
+    {
+        if(kernel_mem_pool.release_frame(i) == ERROR)
+        {
+            if(i < 555 || i >576)
+                failed++;
+        }
+    }
+    if(failed == 0){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else{
+        Console::puti(failed);
+        Console::puts(" Failure Besides Inaccessible\n");
+    }
+    //release inaccessible
+    Console::puts("Release Inaccessible\t");
+    if(kernel_mem_pool.release_frame(556) == ERROR){
+        Console::puts("Pass\n");
+        pass++;
+    }
+    else
+        Console::puts("Failed\n");
+
+    Console::puts("Total Pass:\t");
+    Console::puti(pass);
+    Console::puts("/10\n");
+
+
+    for(;;);
+
+    /* -- WE DO THE FOLLOWING TO KEEP THE COMPILER HAPPY. */
+    return 1;
+}
